342740d46f66fdcf4c3d51a66fe1e3210679b027
dazhangyu212 <dazhangyu212@163.com>
2016-03-16 20:01:07 +0800

    页面初搭建

--- src/com/stone/ordering/activity/ChargeActivity.java
+++ src/com/stone/ordering/activity/ChargeActivity.java
@@ -1,6 +1,15 @@
 package com.stone.ordering.activity;
 
+import com.stone.ordering.R;
+
+import android.os.Bundle;
 
 public class ChargeActivity extends BaseActivity {
 
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_charge);
+	}
+	
 }
--- src/com/stone/ordering/activity/LoginActivity.java
+++ src/com/stone/ordering/activity/LoginActivity.java
@@ -1,5 +1,41 @@
 package com.stone.ordering.activity;
 
-public class LoginActivity extends BaseActivity {
+import com.stone.ordering.R;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.view.View.OnClickListener;
+import android.widget.EditText;
+
+public class LoginActivity extends BaseActivity implements OnClickListener{
+	private EditText etUsername;
+	private EditText etPassword;
+	
+	
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_login);
+		initView();
+	}
+
+	private void initView() {
+		findViewById(R.id.btn_login).setOnClickListener(this);
+		etUsername = (EditText) findViewById(R.id.et_username);
+		etPassword = (EditText) findViewById(R.id.et_password);
+	}
+
+	@Override
+	public void onClick(View v) {
+		switch (v.getId()) {
+		case R.id.btn_login:
+			Intent intent = new Intent(this, MainActivity.class);
+			startActivity(intent);
+			break;
+		default:
+			break;
+		}
+	}
 	
 }
